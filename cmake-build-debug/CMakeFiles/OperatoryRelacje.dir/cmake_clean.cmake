b075e6a75baeb2cd5967832e0438ef1f55af5012
dradkowski <cyberdogpolska@gmail.com>
2018-10-12 14:54:55 +0200

    pierwszy

---
+++ cmake-build-debug/CMakeFiles/OperatoryRelacje.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OperatoryRelacje.dir/main.c.o"
+  "OperatoryRelacje.pdb"
+  "OperatoryRelacje"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OperatoryRelacje.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int a = 5;
+    int b = 7;
+
+    printf("%i", a==b);
+
+    return 0;
+}
