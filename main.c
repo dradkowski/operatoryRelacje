eb357531f3209c01bb8afde185b431071e3b6d84
dradkowski <cyberdogpolska@gmail.com>
2018-10-12 15:06:19 +0200

    druga

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 
 int main() {
     int a = 5;
-    int b = 7;
+    int b = 5;
 
-    printf("%i", a==b);
+    double c = 0.00000001;
+    double d = 0.000000000001;
+    double eps = 0.001;
+
+    printf("%i\n", a==b);
+
+    printf("%i\n", (c - d) * (c -d) < eps);
 
     return 0;
 }
